25a05100f7a8d680fec62a1804dfd62501e18331
Michael <vbifrhenyfdctulf@mail.ru>
2023-10-17 23:40:26 +0300

    Надо проверить.

--- main.cpp
+++ main.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
-#include <vector>
 #include <fstream>
+#include <numeric>
+#include <iterator>
+#include <vector>
 #include <algorithm>
 
 int main(int argc, char* argv[]) {
-    // Open the input file
-    std::ifstream input_file(argv[1]);
-    if (!input_file.is_open()) {
-        std::cerr << "Unable to open input file" << std::endl;
-        return 1;
+    std::fstream inFile(argv[1]); // Open the input file
+    if (!inFile.is_open()) {
+        std::cerr << "Unable to open file";
+        return 1;   // Return with error
     }
-
-    // Read the first line and get the count of numbers
-    int count;
-    input_file >> count;
-
-    // Read the next line and get the numbers
-    std::vector<int> numbers(count);
-    for (int i = 0; i < count; ++i) {
-        input_file >> numbers[i];
+    int N;
+    inFile >> N;
+    std::vector<int> numbers(N);
+    for (int i = 0; i < N; i++) {
+        inFile >> numbers[i];
     }
-
-    // Sort the numbers in ascending order
     std::sort(numbers.begin(), numbers.end());
 
-    // Calculate the maximum win
-    int max_win = numbers[count - 1] + numbers[count - 2];
+    int mid = N / 2;
+    std::vector<int> small(numbers.begin(), numbers.begin() + mid);
+    std::vector<int> large(numbers.begin() + mid, numbers.end());
 
-    // Write the maximum win to the output file
-    std::ofstream output_file(argv[2]);
-    if (!output_file.is_open()) {
-        std::cerr << "Unable to open output file" << std::endl;
-        return 1;
+    int sum_small = std::accumulate(small.begin(), small.end(), 0);
+    int sum_large = std::accumulate(large.begin(), large.end(), 0);
+
+    int result = sum_large - sum_small;
+
+    std::ofstream outFile(argv[2]); // Open the output file
+    if (!outFile.is_open()) {
+        std::cerr << "Unable to open file";
+        return 1;   // Return with error
     }
-    output_file << max_win << std::endl;
+
+    outFile << result;
+    outFile.close();
 
     return 0;
 }
